b36b188d9b925a57e4f80283224d865eddbecf32
pablodamascenoo <pablofacex@gmail.com>
2022-05-04 11:39:56 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { BrowserRouter, Router, Route } from "react-router-dom"
+import Reset from "../assets/global_styles/Reset"
+
+export default function App(){
+
+    return (
+        <>
+            <Reset/>
+        </>
+    )
+}
