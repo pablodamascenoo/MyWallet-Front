11eafbaa3e4dfd55909e72bbde178c7e393e808d
pablodamascenoo <pablofacex@gmail.com>
2022-05-04 12:26:22 -0300

    feat: added routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
-import React from "react"
-import { BrowserRouter, Router, Route } from "react-router-dom"
-import Reset from "../assets/global_styles/Reset"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "../assets/global_styles/GlobalStyle";
+import Reset from "../assets/global_styles/Reset";
+import LoginScreen from "./LoginScreen/LoginScreen";
 
-export default function App(){
-
-    return (
-        <>
-            <Reset/>
-        </>
-    )
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Reset />
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
